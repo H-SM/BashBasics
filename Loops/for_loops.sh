1d9ac131804ee4f03cfeb63e2995a85e9cce88f9
H-SM <harmanmalht777@gmail.com>
2024-01-07 17:27:58 +0530

    For loops in Bash

--- Loops/for_loops.sh
+++ Loops/for_loops.sh
@@ -36,3 +36,41 @@
 #     --- 
 # done
 
+# for i in 1 2 3 4 5
+# do 
+#     echo $i
+# done
+# echo ${BASH_VERSION}
+
+# for i in {10..1..2} #{START..END..INCREMENT}
+# do 
+#     echo $i
+# done
+
+for ((i=0;i<5;i++)) 
+do 
+    echo $i
+done
+
+# USING FOR LOOPS WITH COMMANDS 
+echo 
+
+for command in ls pwd date
+do 
+    echo ------$command------- 
+    $command 
+done
+
+echo 
+
+for item in *
+do 
+    if [ -d $item ] # if the sub-item is a directory
+    then
+        echo $item 
+    fi
+    if [ -f $item ] # if the sub-item is a file
+    then
+        echo "|- $item" 
+    fi
+done
